867c381f7b87056bb844adfffec4aa795d6f3946
Gerardo Lanza <glanza007@gmail.com>
2023-09-04 12:07:10 -0600

    Create Project

---
+++ REQ001/REQ001/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace REQ001.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
